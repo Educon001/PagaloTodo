1d76169803d10c31a9c33120c416f93eaeb83ba4
Eduardo Consalvo <educon001@gmail.com>
2023-05-09 14:46:14 -0400

    fix[PRO-41] UnitTest de GetProviders con respuesta Ok #PRO-20

--- src/pagalotodo-ucab-ms/UCABPagaloTodoMS.Tests/DataSeed/DataSeed.cs
+++ src/pagalotodo-ucab-ms/UCABPagaloTodoMS.Tests/DataSeed/DataSeed.cs
@@ -1,13 +1,51 @@
-﻿using Moq;
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
 using UCABPagaloTodoMS.Core.Database;
+using UCABPagaloTodoMS.Core.Entities;
 
 namespace UCABPagaloTodoMS.Tests.DataSeed
 {
     public static class DataSeed
     {
+        public static Mock<DbSet<ProviderEntity>> mockSetProviderEntity = new Mock<DbSet<ProviderEntity>>();
         public static void SetupDbContextData(this Mock<IUCABPagaloTodoDbContext> mockContext)
         {
+            var providers = new List<ProviderEntity>()
+            {
+                new ProviderEntity()
+                {
+                    Id = Guid.NewGuid(),
+                    CreatedAt = DateTime.Now,
+                    Username = "prueba",
+                    PasswordHash = "Password",
+                    Email = "prueba@prueba.com",
+                    Name = "Jhonny",
+                    LastName = "Test",
+                    Status = true,
+                    Rif = "V123456789",
+                    AccountNumber = "012345678909876543212"
+                },
+                new ProviderEntity()
+                {
+                    Id = Guid.NewGuid(),
+                    CreatedAt = DateTime.Now,
+                    Username = "test",
+                    PasswordHash = "Password",
+                    Email = "test@test.com",
+                    Name = "Juan",
+                    LastName = "Parcial",
+                    Status = true,
+                    Rif = "V123456729",
+                    AccountNumber = "012345578909876543212"
+                }
+            };
+            
+            mockSetProviderEntity.As<IQueryable<ProviderEntity>>().Setup(m => m.Provider).Returns(providers.AsQueryable().Provider);
+            mockSetProviderEntity.As<IQueryable<ProviderEntity>>().Setup(m => m.Expression).Returns(providers.AsQueryable().Expression);
+            mockSetProviderEntity.As<IQueryable<ProviderEntity>>().Setup(m => m.ElementType).Returns(providers.AsQueryable().ElementType);
+            mockSetProviderEntity.As<IQueryable<ProviderEntity>>().Setup(m => m.GetEnumerator()).Returns(providers.GetEnumerator());
 
+            mockContext.Setup(c => c.Providers).Returns(mockSetProviderEntity.Object);
         }
     }
 }
---
+++ src/pagalotodo-ucab-ms/UCABPagaloTodoMS.Tests/UnitTests/Controllers/ProvidersControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+using UCABPagaloTodoMS.Application.Mappers;
+using UCABPagaloTodoMS.Application.Queries;
+using UCABPagaloTodoMS.Application.Responses;
+using UCABPagaloTodoMS.Controllers;
+using UCABPagaloTodoMS.Core.Database;
+using Xunit;
+
+namespace UCABPagaloTodoMS.Tests.UnitTests.Controllers;
+
+public class ProvidersControllerTest
+{
+    private readonly ProvidersController _controller;
+    private readonly Mock<IMediator> _mediatorMock;
+    private readonly Mock<ILogger<ProvidersController>> _loggerMock;
+    private readonly Mock<IUCABPagaloTodoDbContext> _mockContext;
+
+    public ProvidersControllerTest()
+    {
+        _loggerMock = new Mock<ILogger<ProvidersController>>();
+        _mediatorMock = new Mock<IMediator>();
+        _controller = new ProvidersController(_loggerMock.Object, _mediatorMock.Object);
+        _controller.ControllerContext = new ControllerContext();
+        _controller.ControllerContext.HttpContext = new DefaultHttpContext();
+        _controller.ControllerContext.ActionDescriptor = new ControllerActionDescriptor();
+        _mockContext = new Mock<IUCABPagaloTodoDbContext>();
+    }
+
+    [Fact]
+    public async void GetProviders_Returns_Ok()
+    {
+        DataSeed.DataSeed.SetupDbContextData(_mockContext);
+        var expectedResponse = DataSeed.DataSeed.mockSetProviderEntity.Object.Select(p => ProviderMapper.MapEntityToResponse(p)).ToList();
+        _mediatorMock.Setup(m => m.Send(It.IsAny<ProvidersQuery>(), CancellationToken.None)).ReturnsAsync(expectedResponse);
+        var response = await _controller.GetProviders();
+        var okResult = Assert.IsType<OkObjectResult>(response.Result);
+        Assert.IsType<List<ProviderResponse>>(okResult.Value);
+        Assert.Equal(okResult.Value, expectedResponse);
+    }
+}
