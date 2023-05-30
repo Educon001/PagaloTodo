305ff73b1b389b9cc15db31afe7546bf36d18d66
Eduardo Consalvo <educon001@gmail.com>
2023-05-30 16:22:28 -0400

    Remove cascade on delete for payments

--- src/pagalotodo-ucab-ms/UCABPagaloTodoMS.Core/Entities/PaymentEntity.cs
+++ src/pagalotodo-ucab-ms/UCABPagaloTodoMS.Core/Entities/PaymentEntity.cs
@@ -13,9 +13,7 @@ public class PaymentEntity : BaseEntity
     public string? CardSecurityCode { get; set; }
     public string? TransactionId { get; set; }
     public PaymentStatusEnum? PaymentStatus { get; set; }
-    [Required]
     public ConsumerEntity? Consumer { get; set; }
-    [Required]
     public ServiceEntity? Service { get; set; }
     //Para los pagos por confirmacion
     public string? Identifier { get; set; }
